4d523167e70f4cfda748cdcfa73350643bde4f24
Vanshchauhan-1106 <Vanshchauhan1106@gmail.com>
2025-01-08 11:42:41 +0530

    Add Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,9 +6,82 @@ const projects = [
     title: "EasyWrite - Blog App",
     description:
       "EasyWrite is a user-friendly blog web app that allows users to easily create, read, update and delete blog posts. It features a clean and modern design, providing a seamless writing and reading experience. ",
-    image: "/easywrite.png",
+    image: "ProjectsImg/easywrite.png",
     repositoryLink: "https://github.com/VanshChauhanGit/EasyWrite-Blog-App",
     liveLink: "https://easywrite.netlify.app/",
+  },
+  {
+    title: "DoQueue - Todo App",
+    description:
+      "A streamlined to-do list web app designed to enhance productivity and task management. DoQueue allows users to create, organize, and prioritize tasks with an intuitive interface. The app ensures a smooth user experience with features like task status updates and real-time synchronization. Built with modern web technologies and hosted on Netlify for seamless accessibility.",
+    image: "ProjectsImg/doqueue.png",
+    repositoryLink: "https://github.com/VanshChauhanGit/DoQueue-React-Todo-App",
+    liveLink: "https://doqueue.netlify.app/",
+  },
+  {
+    title: "GlobalRates - Realtime Currency Converter App",
+    description:
+      "A web app that provides real-time currency conversion rates, enabling users to effortlessly convert between multiple currencies. Built with a focus on user-friendly design and accurate financial data, this app ensures seamless functionality for international travelers, businesses, and finance enthusiasts.",
+    image: "ProjectsImg/globalrates.png",
+    repositoryLink:
+      "https://github.com/VanshChauhanGit/GlobalRates-Currency-Converter",
+    liveLink: "https://globalrates.netlify.app/",
+  },
+  {
+    title: "Weather's Hub - Real-Time Weather Web App",
+    description:
+      "A user-friendly web app that provides accurate, real-time weather updates for any location. Weather's Hub is designed with a clean and intuitive interface, making it easy to check current weather conditions and forecasts.",
+    image: "ProjectsImg/weathershub.png",
+    repositoryLink: "https://github.com/VanshChauhanGit/WeathersHub",
+    liveLink: "https://weathershub.netlify.app/",
+  },
+  {
+    title: "PassGen Pro - Password Generator App",
+    description:
+      "A lightweight and efficient web app for creating strong, random passwords to enhance online security. PassGen Pro empowers users to generate customized passwords based on length, character types, and other preferences, ensuring their digital safety.",
+    image: "ProjectsImg/passgenpro.png",
+    repositoryLink: "https://github.com/VanshChauhanGit/Pass_Generator",
+    liveLink: "https://pass-gen-pro.netlify.app/",
+  },
+  {
+    title: "RedStorePro - E-Commerce Frontend App",
+    description:
+      "A sleek and visually appealing e-commerce website designed to showcase products and provide an engaging shopping experience. RedStore Pro focuses on delivering a responsive and interactive user interface, making it ideal for modern online stores.",
+    image: "ProjectsImg/redstorepro.png",
+    repositoryLink: "https://github.com/VanshChauhanGit/RedStorePro",
+    liveLink: "https://redstorepro.netlify.app/",
+  },
+  {
+    title: "SimonSays Pro - Classic Memory Game",
+    description:
+      "A fun and interactive web-based memory game that challenges users to follow and repeat increasingly complex patterns of lights and sounds. SimonSays Pro offers a nostalgic gaming experience with a modern, responsive design.",
+    image: "ProjectsImg/simonsayspro.png",
+    repositoryLink: "https://github.com/VanshChauhanGit/simon-says-game",
+    liveLink: "https://simonsayspro.netlify.app/",
+  },
+  {
+    title: "LeadGen - Landing Page for Headphone Promotion",
+    description:
+      "A visually stunning and responsive landing page designed to promote headphones with a modern and professional look. LeadGen highlights the product's key features, design, and user experience, making it an ideal choice for showcasing premium audio devices.",
+    image: "ProjectsImg/leadgen.png",
+    repositoryLink: "https://github.com/VanshChauhanGit/LeadGen",
+    liveLink: "",
+  },
+  {
+    title: "LuxeLiving - Home Listing App",
+    description:
+      "A feature-rich web application that allows users to explore, list, and discover homes effortlessly. LuxeLiving is designed to simplify the home-hunting experience by offering comprehensive property details.",
+    image: "ProjectsImg/luxeliving.png",
+    repositoryLink: "https://github.com/VanshChauhanGit/LuxeLiving",
+    liveLink: "",
+  },
+  {
+    title: "MarketPulse - Realtime Stock Information App",
+    description:
+      "A powerful web application that provides users with up-to-date stock market data, including live stock prices, percentage changes, and market trends. MarketPulse is ideal for investors and traders seeking quick insights and tracking stock performance in real time.",
+    image: "ProjectsImg/marketpulse.png",
+    repositoryLink: "https://github.com/VanshChauhanGit/market-pulse",
+    liveLink: "",
   },
 ];
 
